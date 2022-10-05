53cd650c7e4165e8b91740351ed2baef0414edc1
Tenzing Sherpa <tsherpa2016@gmail.com>
2022-10-05 11:17:50 -0400

    Fixed merge conflicts

--- server/api/index.js
+++ server/api/index.js
@@ -2,5 +2,10 @@ const router = require('express').Router();
 
 // router.use('/template', require('./template'));
 router.use('/user', require('./user'));
+router.use('/recipes', require('./recipes'));
+router.use('/cuisines', require('./cuisines'));
+router.use('/restrictions', require('./restrictions'));
+router.use('/ingredients', require('./ingredients'));
+router.use('/appliances', require('./appliances'));
 
 module.exports = router;
