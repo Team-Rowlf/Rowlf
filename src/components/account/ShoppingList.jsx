e5b6aa89e522ee0b54b43b628bfb943c7667f6d7
Tenzing Sherpa <tsherpa2016@gmail.com>
2022-10-07 15:46:38 -0400

    Fixed ingredient qty displaying too many digits

--- src/components/account/ShoppingList.jsx
+++ src/components/account/ShoppingList.jsx
@@ -1,35 +1,43 @@
 import React from 'react';
 
 const ShoppingList = () => {
+  // let shoppingList = [{name: 'Recipe 1', ingredients:[{name:'Ingredient 1', quantity: 1},{name:'Ingredient 2', quantity: 1},{name:'Ingredient 3', quantity: 1},{name:'Ingredient 4', quantity: 1}]},{name: 'Recipe 2', ingredients:[{name:'Ingredient 1', quantity: 1},{name:'Ingredient 2', quantity: 1},{name:'Ingredient 3', quantity: 1},{name:'Ingredient 4', quantity: 1}]},{name: 'Recipe 3', ingredients:[{name:'Ingredient 1', quantity: 1},{name:'Ingredient 2', quantity: 1},{name:'Ingredient 3', quantity: 1},{name:'Ingredient 4', quantity: 1}]},{name: 'Recipe 4', ingredients:[{name:'Ingredient 1', quantity: 1},{name:'Ingredient 2', quantity: 1},{name:'Ingredient 3', quantity: 1},{name:'Ingredient 4', quantity: 1}]}]
 
-	// let shoppingList = [{name: 'Recipe 1', ingredients:[{name:'Ingredient 1', quantity: 1},{name:'Ingredient 2', quantity: 1},{name:'Ingredient 3', quantity: 1},{name:'Ingredient 4', quantity: 1}]},{name: 'Recipe 2', ingredients:[{name:'Ingredient 1', quantity: 1},{name:'Ingredient 2', quantity: 1},{name:'Ingredient 3', quantity: 1},{name:'Ingredient 4', quantity: 1}]},{name: 'Recipe 3', ingredients:[{name:'Ingredient 1', quantity: 1},{name:'Ingredient 2', quantity: 1},{name:'Ingredient 3', quantity: 1},{name:'Ingredient 4', quantity: 1}]},{name: 'Recipe 4', ingredients:[{name:'Ingredient 1', quantity: 1},{name:'Ingredient 2', quantity: 1},{name:'Ingredient 3', quantity: 1},{name:'Ingredient 4', quantity: 1}]}]
+  return (
+    <div className="shoppingList-container">
+      <h1>
+        <u>Main Shopping List</u>
+      </h1>
 
-
-	return (
-		<div className="shoppingList-container">
-			<h1>
-				<u>Main Shopping List</u>
-			</h1>
-
-			{shoppingList.map(recipe=>{
-					const ingredients = recipe.lineItems
-					return(
-						<div className="recipe" key={recipe.name}>
-							<input type="checkbox" className="recipe-checkbox" value="Recipe1.name"/> {recipe.name}
-							<div className="ingredients">
-								{ingredients.map(lineItem=>{
-									return(
-										<div className="ingredient-checkbox" key={lineItem.ingredient.name}>
-											<input type="checkbox" value={lineItem.ingredient.name}/> (x{lineItem.quantity} {lineItem.measurement}) {lineItem.ingredient.name}
-										</div>
-									)
-								})}
-							</div>
-						</div>
-					)
-				})}
-		</div>
-	);
+      {shoppingList.map((recipe) => {
+        const ingredients = recipe.lineItems;
+        return (
+          <div className="recipe" key={recipe.name}>
+            <input
+              type="checkbox"
+              className="recipe-checkbox"
+              value="Recipe1.name"
+            />{' '}
+            {recipe.name}
+            <div className="ingredients">
+              {ingredients.map((lineItem) => {
+                return (
+                  <div
+                    className="ingredient-checkbox"
+                    key={lineItem.ingredient.name}
+                  >
+                    <input type="checkbox" value={lineItem.ingredient.name} />{' '}
+                    (x{lineItem.quantity} {lineItem.measurement}){' '}
+                    {lineItem.ingredient.name}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ShoppingList;
